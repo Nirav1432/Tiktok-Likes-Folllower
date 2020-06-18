80277627811d018abd7a779f84488e918438ecef
Niravkumar Bhesaniya <nirav.bhesaniya@artoon.in>
2020-06-18 23:23:18 +0530

    Update watchTimerStyles.js

--- src/Screens/styles/watchTimerStyles.js
+++ src/Screens/styles/watchTimerStyles.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     Timer: { height: hp(8.5), flexDirection: "row", width: wp(60), padding: hp(1.5), backgroundColor: "#FE2C55", borderRadius: hp(100), elevation: 10, alignSelf: "center", marginTop: hp(8), alignItems: "center" },
     watch: { height: hp(6), width: hp(6) },
     watchView: { justifyContent: "center", alignItems: "center", position: "absolute", left: hp(2) },
-    timerTExt: { fontFamily: Fonts.LatoBlack, color: "white", fontSize: hp(3) },
+    timerTExt: { fontFamily: Fonts.LatoBlack, color: "white", fontSize: hp(3), left: wp(1) },
 
     IMG1: { height: hp(3.5), width: hp(3.5) },
     watchView2: { justifyContent: "center", height: hp(6), width: hp(6), borderColor: "white", alignItems: "center", position: "absolute", left: hp(2), borderRadius: hp(100), borderWidth: hp(0.3) },
